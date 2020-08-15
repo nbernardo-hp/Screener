bf9fed4f861898fdb9abd831d6ea95f70a7d69ff
nbernardo-hp <nbernardo0610@starkstate.net>
2020-08-15 17:32:21 -0400

    Form to select specific browser at begining of application

---
+++ frmSelectBrowser.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Screener
+{
+    partial class frmSelectBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChrome = new System.Windows.Forms.Button();
+            this.btnFirefox = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnChrome
+            // 
+            this.btnChrome.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnChrome.Location = new System.Drawing.Point(48, 66);
+            this.btnChrome.Name = "btnChrome";
+            this.btnChrome.Size = new System.Drawing.Size(75, 23);
+            this.btnChrome.TabIndex = 0;
+            this.btnChrome.Text = "Chrome";
+            this.btnChrome.UseVisualStyleBackColor = true;
+            this.btnChrome.Click += new System.EventHandler(this.btnChrome_Click);
+            // 
+            // btnFirefox
+            // 
+            this.btnFirefox.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnFirefox.Location = new System.Drawing.Point(159, 66);
+            this.btnFirefox.Name = "btnFirefox";
+            this.btnFirefox.Size = new System.Drawing.Size(75, 23);
+            this.btnFirefox.TabIndex = 1;
+            this.btnFirefox.Text = "Firefox";
+            this.btnFirefox.UseVisualStyleBackColor = true;
+            this.btnFirefox.Click += new System.EventHandler(this.btnFirefox_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(270, 66);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "This application requires either Google Chrome of Mozilla Firefox to operate.\r\nSe" +
+    "lect the browser that you have installed to use.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frmSelectBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 105);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFirefox);
+            this.Controls.Add(this.btnChrome);
+            this.Name = "frmSelectBrowser";
+            this.Text = "Select A Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChrome;
+        private System.Windows.Forms.Button btnFirefox;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label1;
+    }
+}

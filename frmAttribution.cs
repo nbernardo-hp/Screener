0cec105acba64d41e90da87f7972b0e7956c081f
nbernardo-hp <58055338+nbernardo-hp@users.noreply.github.com>
2020-08-19 14:24:04 -0400

    Add files via upload

---
+++ frmAttribution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Screener
+{
+    public partial class frmAttribution : Form
+    {
+        public frmAttribution()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.screenerIcon;
+        }//end default constructor
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }//end btnOK_Click
+    }//end class
+}//end namespace
